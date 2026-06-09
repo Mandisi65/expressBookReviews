2347b238f85aa39e990dbac0dd62e43c303edda1
Mandisi65 <mandisitshezi17@gmail.com>
2026-06-09 22:43:25 +0200

    Task 3 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,6 +7,7 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -55,9 +56,34 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const authorName = req.params.author.toLowerCase(); // Convert parameter to lowercase for case-insensitive matching
+
+  // Creating a promise context to filter entries non-blockingly (Task 12 requirement)
+  const fetchBooksByAuthor = new Promise((resolve, reject) => {
+    const keys = Object.keys(books);
+    const matchingBooks = {};
+
+    keys.forEach((key) => {
+      if (books[key].author.toLowerCase() === authorName) {
+        matchingBooks[key] = books[key];
+      }
+    });
+
+    if (Object.keys(matchingBooks).length > 0) {
+      resolve(matchingBooks);
+    } else {
+      reject({ message: `No books found matching author: ${req.params.author}` });
+    }
+  });
+
+  fetchBooksByAuthor
+    .then((filteredBooks) => {
+      return res.status(200).json(filteredBooks);
+    })
+    .catch((error) => {
+      return res.status(404).json(error);
+    });
 });
 
 // Get all books based on title
